3092ecaa5c9fc9124657ac1e3826c2726af8acd6
uladzimirAAsanko <volodyasn@gmail.com>
2021-05-10 05:20:39 +0300

    added calculations

--- src/main/java/by/sanko/joiner/main/Main.java
+++ src/main/java/by/sanko/joiner/main/Main.java
@@ -85,6 +85,7 @@ public class Main {
                 WeatherData data = WeatherParser.parseData(value);
                 System.out.println(data.toString());
                 HashMap<String, Pair<Double, Integer>> map =  listOfMaps.get(data.getWeatherDate());
+                System.out.println(map.keySet());
                 Pair<Double,Integer> pair = map.get(data.getGeoHash());
                 if(pair != null) {
                     Double avg_temp = (Double) pair.getLeft();
